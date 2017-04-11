8912bd9220542a1411d0dbf571cb7560c71aa0eb
Lorenzo Boyice <lorenzo.boyice@garmin.com>
2017-04-11 17:16:15 -0500

    Trying to get this two way street going

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -68,17 +68,36 @@ function takeImage() {
 }
 
 var myPhotoBrowserDark = myApp.photoBrowser({
-    photos : [
-        {
+    photos: [{
             html: '<iframe width="560" height="315" src="https://www.youtube.com/embed/92ISlO9U-84" style="height: 70%" frameborder="0" allowfullscreen></iframe>',
             caption: 'zotime :)'
         },
         'http://lorempixel.com/1024/1024/sports/1/',
-        {url: imageUrl, caption: 'Sauce!'},
+        {
+            url: imageUrl,
+            caption: 'Sauce!'
+        },
         'http://lorempixel.com/1024/1024/sports/2/',
-        {url: imageUrl, caption: 'Sauce!'},{url: imageUrl, caption: 'Sauce!'},
-        {url:'http://lorempixel.com/1024/1024/sports/3/'},
-        {url: imageUrl, caption: 'Sauce!'},{url: imageUrl, caption: 'Sauce!'},{url: imageUrl, caption: 'Sauce!'},
+        {
+            url: imageUrl,
+            caption: 'Sauce!'
+        }, {
+            url: imageUrl,
+            caption: 'Sauce!'
+        },
+        {
+            url: 'http://lorempixel.com/1024/1024/sports/3/'
+        },
+        {
+            url: imageUrl,
+            caption: 'Sauce!'
+        }, {
+            url: imageUrl,
+            caption: 'Sauce!'
+        }, {
+            url: imageUrl,
+            caption: 'Sauce!'
+        },
     ],
     theme: 'dark'
 });
@@ -89,31 +108,86 @@ $$('.pb-standalone-dark').on('click', function () {
 var count = 0;
 var rowCount = -1;
 var ROW_ID = "#photoUploads" + rowCount;
+var COL_COUNT = 3;
+var physicalScreenWidth = window.screen.width;
+var physicalScreenHeight = window.screen.height;
+var ALBUM = "uploads"
+var pickerDevice = myApp.picker({
+    input: '#picker-device',
+    toolbarTemplate: '<div class="toolbar theme-teal">' +
+        '<div class="toolbar-inner">' +
+        '<div class="right">' +
+        '<a href="#" class="link close-picker">Done</a>' +
+        '</div>' +
+        '</div>' +
+        '</div>',
+    cols: [{
+        textAlign: 'center',
+        values: ['Family Reunion 2018', 'iPhone 4S', 'iPhone 5', 'iPhone 5S', 'iPhone 6', 'iPhone 6 Plus', 'iPad 2', 'iPad Retina', 'iPad Air', 'iPad mini', 'iPad mini 2', 'iPad mini 3']
+    }],
+    onChange: function (picker, album) {
+        ALBUM = album
+    },
+    onClose: function(picker){
+        httpGetAsync("http://zotime.ddns.net/pd/photoUpload.php", function(resp){
+            console.log(resp)
+            alert(resp)
+        })
+    }
+});
+
+function httpGetAsync(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl+"?album="+ALBUM, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
 
 function iGotTheSauce() {
-    if (count % 3 == 0) {
+    if (count % COL_COUNT == 0) {
         rowCount++;
         ROW_ID = "#photoUploads" + rowCount
         nextRow = document.createElement("div")
         nextRow.setAttribute("class", "row")
-        nextRow.setAttribute("id","photoUploads" + rowCount)
-        nextRow.setAttribute("style", "min-height:100px; padding:5px")
+        nextRow.setAttribute("id", "photoUploads" + rowCount)
+        nextRow.setAttribute("style", "min-height:100px; padding:5px; text-align:center;")
         $$("#inner-body").append(nextRow)
 
-        for (i = 0; i < 3; i++) {
+        for (i = 0; i < COL_COUNT; i++) {
             imgSlot = document.createElement("div")
             imgSlot.setAttribute("id", "div" + (count + i));
             imgSlot.setAttribute("class", "short-loder col-auto")
             //alert("ROW_ID: "+ROW_ID+" CONTAIN_ID: #div"+(count+i))
             $$(ROW_ID).append(imgSlot)
-            
-            image = document.createElement("img")
-            image.setAttribute("src", imageUrl)
-            image.setAttribute("style", "max-height: 150px")
-            $$("#div" + (count + i)).append(image)
+            placePic(count + i)
+
         }
         count += 3
     }
+}
+
+function placePic(num) {
+    //console.log(physicalScreenWidth)
+    loadImage(
+        imageUrl,
+        function (img) {
+            if (img.type === "error") {
+                console.log("Error loading image " + imageUrl);
+            } else {
+                //alert("CONTAIN_ID: " + "#div" + (num))
+                img.setAttribute("style", "margin:auto;")
+                //console.log(img)
+                $$("#div" + (num)).append(img);
+            }
+        }, {
+            maxWidth: physicalScreenWidth / 3.5,
+            maxHeight: physicalScreenHeight / 3.5,
+            crop: true
+        }
+    );
 }
 
 function uploadPhoto(imageURI) {
@@ -150,7 +224,7 @@ function uploadPhoto(imageURI) {
             if (img.type === "error") {
                 console.log("Error loading image " + imageURI);
             } else {
-                alert("CONTAIN_ID: " + CONTAIN_ID)
+                //alert("CONTAIN_ID: " + CONTAIN_ID)
                 $$(CONTAIN_ID).append(img);
             }
         }, {
@@ -180,14 +254,17 @@ function uploadPhoto(imageURI) {
     options.fileKey = "file";
     options.fileName = imageURI.substr(imageURI.lastIndexOf('/') + 1);
     options.mimeType = "image/jpeg";
-    console.log(options.fileName);
+    options.fileName = options.fileName.split("?")[0]
+    alert(options.fileName);
     var params = new Object();
-    params.value1 = "test";
+    params.album = ALBUM;
+    alert(ALBUM)
     params.value2 = "param";
     options.params = params;
     options.chunkedMode = false;
     ft.upload(imageURI, "http://zotime.ddns.net/pd/photoUpload.php", function (result) {
         console.log(JSON.stringify(result));
+        alert(JSON.stringify(result))
     }, function (error) {
         console.log(JSON.stringify(error));
     }, options);
