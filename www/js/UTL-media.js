8335268b62b4764b95042ccf17859d9f86dc4b08
renzob.kc <renzob.kc@gmail.com>
2017-09-15 16:04:30 -0400

    switched out go backend port

--- www/js/UTL-media.js
+++ www/js/UTL-media.js
@@ -49,7 +49,7 @@ function browserFileTransfer(fl, albumId) {
             console.log(byteLength(fr.result))
 
             //postReq(PHOTO_SERVICE, _data, success, "send photo")
-            postReq("http://zotime.ddns.net:8081/photo", _data, success, "send photo to go backend")
+            postReq("http://zotime.ddns.net:2500/photo", _data, success, "send photo to go backend")
         }
     }
     readBlob(fl, fr, bLoaded);
