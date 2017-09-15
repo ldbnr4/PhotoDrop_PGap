86fcac65578e3626f8dc0cc7502418433f2f2687
renzob.kc <renzob.kc@gmail.com>
2017-09-15 13:34:17 -0400

    want to see file size

--- www/js/UTL-media.js
+++ www/js/UTL-media.js
@@ -35,11 +35,25 @@ function sendFileToServ(fl, albumId) {
                 console.log("XHR: "+xhr);
                 console.log("STATUS: "+status);
             }
+            
+            console.log(fr.result)
 
             $$.post(PHOTO_SERVICE, _data, success, error)
         }
     };
     readBlob(fl, fr, bLoaded);
+}
+
+function byteLength(str) {
+  // returns the byte length of an utf8 string
+  var s = str.length;
+  for (var i=str.length-1; i>=0; i--) {
+    var code = str.charCodeAt(i);
+    if (code > 0x7f && code <= 0x7ff) s++;
+    else if (code > 0x7ff && code <= 0xffff) s+=2;
+    if (code >= 0xDC00 && code <= 0xDFFF) i--; //trail surrogate
+  }
+  return s;
 }
 
 function uploadPhoto(imageURI, albumId) {
